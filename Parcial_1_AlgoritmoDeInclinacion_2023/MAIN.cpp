23ba4a97f384724b3fdf79e8ae75267398575aa3
EDWAR GONZALEZ <yercken30@gmail.com>
2023-10-07 01:19:49 -0500

    INCLINACIÓN RESPECTO AL EJE X - TERMINADO -Se agregó un menú, permitiendo elegir si cambiar coordenadas del rectángulo o el shx

--- Parcial_1_AlgoritmoDeInclinacion_2023/MAIN.cpp
+++ Parcial_1_AlgoritmoDeInclinacion_2023/MAIN.cpp
@@ -2,44 +2,15 @@
 #include <iostream>
 
 // Ancho y alto de la ventana
-const int ventanaAncho = 1280;
+const int ventanaAncho = 850;
 const int ventanaAlto = 720;
 float shx = 0.0f;
 
-
-//ABAJO IZQ
-
+// Coordenadas iniciales
 float x_down_i = 0.0f, y_down_i = 0.0f;
-//ARIBA IZQ
 float x_up_i = 0.0f, y_up_i = 200.0f;
-
-//ABJO DERECHA
 float x_down_d = 150.0f, y_down_d = 0.0f;
-
-//ARRIBA  DERECHA
 float x_up_d = 150.0f, y_up_d = 200.0f;
-
-
-
-
-//// Definición de los vértices del rectángulo (todos positivos)
-//float vertices[] = {
-//    x_up_i  , y_up_i,   // Vértice 1 (arriba izquierda)
-//    x_up_d , y_up_d,     // Vértice 2 (arriba derecha)
-//    x_down_d , y_down_d,  // Vértice 3 (abajo derecha)
-//    x_down_i ,  y_down_i   // Vértice 4 (abajo izquierda)
-//};
-
-
-// Definición de los vértices del rectángulo (todos positivos)
-float vertices[] = {
-    x_up_i   + (shx * y_up_i)  , y_up_i,   // Vértice 1 (arriba izquierda)
-    x_up_d   + (shx * y_up_d)  , y_up_d,     // Vértice 2 (arriba derecha)
-    x_down_d + (shx * y_down_d), y_down_d,  // Vértice 3 (abajo derecha)
-    x_down_i + (shx * y_down_i),  y_down_i   // Vértice 4 (abajo izquierda)
-};
-
-
 
 // Función para centrar la ventana GLFW en la pantalla
 void centrarVentana(GLFWwindow* window) {
@@ -51,26 +22,29 @@ void centrarVentana(GLFWwindow* window) {
 
 // Función para dibujar el rectángulo
 void dibujarRectangulo() {
+
     glBegin(GL_QUADS);
 
     // Color del rectángulo (azul)
     glColor3f(0.0f, 0.0f, 1.0f); // Azul
-
     // Dibuja el rectángulo usando los vértices definidos
-    for (int i = 0; i < 8; i += 2) {
-        glVertex2f(vertices[i], vertices[i + 1]);
-    }
-
+    glVertex2f(x_up_i + (shx * y_up_i), y_up_i); // Vértice 1 (arriba izquierda)
+    glVertex2f(x_up_d + (shx * y_up_d), y_up_d); // Vértice 2 (arriba derecha)
+    glVertex2f(x_down_d + (shx * y_down_d), y_down_d); // Vértice 3 (abajo derecha)
+    glVertex2f(x_down_i + (shx * y_down_i), y_down_i); // Vértice 4 (abajo izquierda)
     glEnd();
 }
 
 void cambiarColorFondo() {
+
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-// Función para dibujar un plano cartesiano como guía con ejes X e Y 
+// Función para dibujar un plano cartesiano como guía con ejes X e Y
 void dibujarPlanoCartesiano(int ancho, int alto, int paso) {
+
     // Dibujar ejes X e Y
     glBegin(GL_LINES);
 
@@ -83,9 +57,34 @@ void dibujarPlanoCartesiano(int ancho, int alto, int paso) {
     glColor3f(1.0f, 0.0f, 0.0f); // Rojo
     glVertex2i(1, -alto);
     glVertex2i(1, alto);
-
     glEnd();
 }
+
+
+void menuCambiarShx() {
+
+    std::cout << "\nIngrese el nuevo valor de shx: ";
+    std::cin >> shx;
+
+    //limpiar pantalla
+    system("cls");
+
+}
+
+void menuCambiarCoordenadas() {
+    std::cout << "\nIngrese el nuevo valor de ( x_down_i ,  y_down_i ) separados por espacio: ";
+    std::cin >> x_down_i >> y_down_i;
+    std::cout << "\nIngrese el nuevo valor de ( x_up_i   ,  y_up_i   ) separados por espacio: ";
+    std::cin >> x_up_i >> y_up_i;
+    std::cout << "\nIngrese el nuevo valor de ( x_down_d ,  y_down_d ) separados por espacio: ";
+    std::cin >> x_down_d >> y_down_d;
+    std::cout << "\nIngrese el nuevo valor de ( x_up_d   , y_up_d    )  separados por espacio: ";
+    std::cin >> x_up_d >> y_up_d;
+
+    //limpiar pantalla
+    system("cls");
+}
+
 
 
 int main() {
@@ -96,7 +95,7 @@ int main() {
         return -1;
     }
 
-    window = glfwCreateWindow(ventanaAncho, ventanaAlto, "OpenGL Rectángulo", NULL, NULL);
+    window = glfwCreateWindow(ventanaAncho, ventanaAlto, "Algoritmos De Inclinación", NULL, NULL);
 
     if (!window) {
         std::cerr << "Error: Fallo en la creación de la ventana GLFW." << std::endl;
@@ -104,7 +103,9 @@ int main() {
         return -1;
     }
 
-    centrarVentana(window);
+    //centrarVentana(window);
+    glfwSetWindowPos(window,0, 100);
+
     glfwMakeContextCurrent(window);
     glOrtho(0, ventanaAncho, 0, ventanaAlto, -1, 1);
 
@@ -117,6 +118,37 @@ int main() {
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        // Mostrar el menú de opciones
+        std::cout << "MENU: \n" << std::endl;
+        std::cout << "1. Cambiar shx" << std::endl;
+        std::cout << "2. Cambiar coordenadas" << std::endl;
+        std::cout << "0. Salir" << std::endl;
+        std::cout << "______________________________________\n\n" << std::endl;
+
+
+        int opcion;
+        std::cout << "OPCION: ";
+        std::cin >> opcion;
+
+        //limpiar pantalla
+        system("cls");
+
+
+        switch (opcion) {
+        case 1:
+            menuCambiarShx();
+            break;
+        case 2:
+            menuCambiarCoordenadas();
+            break;
+        case 0:
+            glfwSetWindowShouldClose(window, true);  // Salir del bucle
+            break;
+        default:
+            std::cout << "Opción no válida. Por favor, elija una opción válida." << std::endl;
+            break;
+        }
     }
 
     glfwTerminate();
